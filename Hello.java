75e097f9d1eda3ffd4c1f9d689e005383918563d
deshmukh9970 <90455776+deshmukh9970@users.noreply.github.com>
2024-12-05 16:52:41 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-class Hello{ 
-public static void main (String args[] ) 
-{ 
-System.out.println("Hello"); 
-System.out.println("Thank You from jenkins");
-}
-}
+class Hello
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
